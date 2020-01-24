2d3241217820c67ab4cb20fdacbabc6bbb2479b3
Kain Wesker <turik3@gmail.com>
2020-01-24 15:31:13 +0400

    pj

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 
 <!-- Scripts -->
-<script src="{{ asset('js/script.js') }}"></script>
-<script src="{{ asset('js/theme.js') }}"></script>
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="js/script.js"></script>
+<script src="js/theme.js"></script>
+<script src="js/app.js"></script>
